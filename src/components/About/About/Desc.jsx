08a7af09573695ef734e3326b3b9f9ddd0ebc2d6
Arcdev <colibaoallanreyes@gmail.com>
2020-01-05 20:25:35 +0800

    commit

--- src/components/About/About/Desc.jsx
+++ src/components/About/About/Desc.jsx
@@ -7,8 +7,8 @@ class Desc extends Component {
     };
     return (
       <p style={style}>
-        Hi, I'm Allan Reyes Colibao, a <strong>tech enthusiast</strong> and has
-        a huge passion on web development and graphic designing.{" "}
+        Hi, I'm Allan Reyes Colibao, a <strong>Tech enthusiast</strong> and has
+        a huge passion on Web development and Graphic designing.{" "}
         <strong>Always on my PC. 24/7 @ my workstation</strong>
       </p>
     );
